c8a07caf25ebcd655d710185017e53f5dcc7f9c6
joaojilbertogil <112709914+joaojilbertogil@users.noreply.github.com>
2023-04-03 14:54:14 -0300

    Add files via upload

---
+++ exercicios/js/exercicio4.js
@@ -0,0 +1,13 @@
+document.write("<h1><center>Os Primeiros 15 numeros da sequencia fibonacci</center></h1>")
+// Definindo Variáveis
+n1 = 0
+n2 = 1
+
+// For para a realização do calculo
+for(var i = 0; i <= 15; i++){
+    soma = n1 + n2;
+    n1 = n2;
+    n2 = soma;
+    document.write("<b>Número: </b>", n1 , "<br><br>");
+}
+document.write('<br> <img src="js/serie.png" alt="Cade?">')
